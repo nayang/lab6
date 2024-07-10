10aeae188f5149213347012e43030433fb532b48
yang0367 <yang0367@algonquinlive.com>
2024-07-10 19:47:12 -0400

    add database function

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
   final List<Todo> _todoItems = [];
   final TextEditingController _controller = TextEditingController();
   late TodoDao _todoDao;
+  Todo? _selectedTodo; // 新增：选中的待办事项
 
   @override
   void initState() {
@@ -70,92 +71,177 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  Future<void> _removeTodoItem(int index) async {
-    final todo = _todoItems[index];
+  Future<void> _deleteTodoItem(Todo todo) async {
     await _todoDao.deleteTodo(todo);
     final items = await _todoDao.findAllTodos();
     setState(() {
       _todoItems.clear();
       _todoItems.addAll(items);
+      _selectedTodo = null; // 删除后清空选中项
     });
   }
 
-  void _promptRemoveTodoItem(int index) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Delete "${_todoItems[index].task}"?'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Cancel'),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            TextButton(
-              child: Text('Yes'),
-              onPressed: () {
-                _removeTodoItem(index);
+  void _showTodoDetails(Todo todo) {
+    setState(() {
+      _selectedTodo = todo;
+    });
+
+    // 检查当前是否为平板横屏模式
+    bool isTabletLandscape = MediaQuery.of(context).size.width > 600 && MediaQuery.of(context).orientation == Orientation.landscape;
+    if (!isTabletLandscape) {
+      // 如果不是平板横屏模式，则导航到详情页面
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TodoDetailsPage(
+            todo: todo,
+            onDelete: () {
+              _deleteTodoItem(todo);
+              // 检查是否可以返回上一页
+              if (Navigator.of(context).canPop()) {
                 Navigator.of(context).pop();
-              },
-            )
-          ],
-        );
-      },
+              }
+            },
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isTabletLandscape = MediaQuery.of(context).size.width > 600 && MediaQuery.of(context).orientation == Orientation.landscape;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Row(
+        children: [
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          controller: _controller,
+                          decoration: InputDecoration(
+                            hintText: 'Enter a todo item',
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        child: Text('Add'),
+                        onPressed: () {
+                          _addTodoItem(_controller.text);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: _todoItems.isEmpty
+                      ? Center(
+                    child: Text('There are no items in the list'),
+                  )
+                      : ListView.builder(
+                    itemCount: _todoItems.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Row number: $index'),
+                            SizedBox(width: 100),
+                            Text(_todoItems[index].task),
+                          ],
+                        ),
+                        onTap: () => _showTodoDetails(_todoItems[index]), // 修改：用点击事件显示详情
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // 如果是平板横屏模式，则显示详情页面
+          if (isTabletLandscape && _selectedTodo != null)
+            VerticalDivider(),
+          if (isTabletLandscape && _selectedTodo != null)
+            Expanded(
+              child: _buildDetailsPage(),
+            ),
+        ],
+      ),
     );
   }
+
+  Widget _buildDetailsPage() {
+    return _selectedTodo == null
+        ? Center(
+      child: Text('No item selected'),
+    )
+        : Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text('Task: ${_selectedTodo!.task}', style: TextStyle(fontSize: 20)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text('ID: ${_selectedTodo!.id}', style: TextStyle(fontSize: 20)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton(
+            child: Text('Delete'),
+            onPressed: () {
+              _deleteTodoItem(_selectedTodo!);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TodoDetailsPage extends StatelessWidget {
+  final Todo todo;
+  final VoidCallback onDelete;
+
+  const TodoDetailsPage({Key? key, required this.todo, required this.onDelete}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Demo Home Page'),
+        title: Text('Todo Details'),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: 'Enter a todo item',
-                    ),
-                  ),
-                ),
-                TextButton(
-                  child: Text('Add'),
-                  onPressed: () {
-                    _addTodoItem(_controller.text);
-                  },
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: _todoItems.isEmpty
-                ? Center(
-              child: Text('There are no items in the list'),
-            )
-                : ListView.builder(
-              itemCount: _todoItems.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Row number: $index'),
-                      SizedBox(width: 100),
-                      Text(_todoItems[index].task),
-                    ],
-                  ),
-                  onLongPress: () => _promptRemoveTodoItem(index),
-                );
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Task: ${todo.task}', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 16),
+            Text('ID: ${todo.id}', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 16),
+            ElevatedButton(
+              child: Text('Delete'),
+              onPressed: () {
+                onDelete();
+                // 检查是否可以返回上一页
+                if (Navigator.of(context).canPop()) {
+                  Navigator.of(context).pop();
+                }
               },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
